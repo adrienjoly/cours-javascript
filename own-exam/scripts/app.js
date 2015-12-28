4df0d54cd8f695ba8fdb45cf3a9070d48b954d75
Adrien Joly <adrien@whyd.com>
2015-12-28 12:18:15 +0100

    for use from adrienjoly.com domain

--- own-exam/scripts/app.js
+++ own-exam/scripts/app.js
@@ -20,7 +20,7 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   if (window.location.port === '') {  // if production
     // Uncomment app.baseURL below and
     // set app.baseURL to '/your-pathname/' if running from folder in production
-    // app.baseUrl = '/polymer-starter-kit/';
+    app.baseUrl = '/cours-javascript/own-exam/';
   }
 
   app.displayInstalledToast = function() {
