da5dfe7b4bf14c21bbc6866a2afc8774971f1266
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-02-06 13:05:33 +0100

    mise a jour solution jeu du pendu

--- exercices/3-solutions/3-pendu-bonus.js
+++ exercices/3-solutions/3-pendu-bonus.js
@@ -1,28 +1,36 @@
-var lettres = [ 's', 'u', 'p', 'p', 'e', 'r' ];
+'use strict';
 
-for (var tentatives = 10; tentatives > 0 && lettres.length > 0; ) {
-  console.log('lettres à trouver:', lettres);
-  alert('Lettres à trouver: ' + lettres.length + '. Tentatives: ' + tentatives);
+var MAX_TENTATIVES = 10;
+var mauvaisesLettres = [ ]; // sera complété pendant le jeu
+var lettresAttendues = [ 'a', 'n', 'n', 'a' ];
+
+for (; mauvaisesLettres.length < MAX_TENTATIVES && lettresAttendues.length > 0; ) {
+  console.log('lettres à trouver:', lettresAttendues);
+  alert('Lettres à trouver: ' + lettresAttendues.length + '. '
+    + 'Tentatives restantes: ' + (MAX_TENTATIVES - mauvaisesLettres.length));
   var lettre = prompt('Devinez une lettre');
-  var trouvees = 0;
+
   // tant qu'on trouve des occurrences, on continue de les supprimer une à une
-  for (var indice = 0; indice !== -1 ;) {
-    indice = lettres.indexOf(lettre); // cherche la prochaine occurrence de la lettre
-    if (indice !== -1) {
-      lettres.splice(indice, 1); // retire cette occurrence de la lettre trouvée dans le tableau
+  var trouvees = 0;
+  for (var indice = 0; indice != -1 ;) {
+    indice = lettresAttendues.indexOf(lettre); // cherche la prochaine occurrence de la lettre
+    if (indice != -1) {
+      lettresAttendues.splice(indice, 1); // retire cette occurrence de la lettre trouvée dans le tableau
       trouvees++;
     }
   }
-  if (trouvees === 0) {
-    --tentatives;
-    alert('Le mot à trouver ne contient pas la lettre ' + lettre+ ', désolé...');
+
+  if (trouvees > 0) {
+    alert('Bonne pioche ! Il y avait bien ' + trouvees + ' fois la lettre '
+      + lettre + ' dans le mot à trouver !');
   } else {
-    alert('Bonne pioche ! Il y avait bien ' + trouvees + ' ' + lettre + ' dans le mot à trouver !');
+    mauvaisesLettres.push(lettre);
+    alert('Le mot à trouver ne contient pas la lettre ' + lettre + ', désolé...');
   }
 }
 
-if (lettres.length === 0) {
-  alert('bravo!');
-} else if (tentatives === 0) {
-  alert('perdu!');
+if (lettresAttendues.length == 0) {
+  alert('Bravo, vous avez trouvé le mot !');
+} else if (mauvaisesLettres.length == MAX_TENTATIVES) {
+  alert('Perdu... Faites une autre partie !');
 }
--- exercices/3-solutions/3-pendu.js
+++ exercices/3-solutions/3-pendu.js
@@ -1,21 +1,26 @@
-var lettres = [ 's', 'u', 'p', 'e', 'r' ];
+'use strict';
 
-for (var tentatives = 10; tentatives > 0 && lettres.length > 0; ) {
-  console.log('lettres à trouver:', lettres);
-  alert('Lettres à trouver: ' + lettres.length + '. Tentatives: ' + tentatives);
+var MAX_TENTATIVES = 10;
+var mauvaisesLettres = [ ]; // sera complété pendant le jeu
+var lettresAttendues = [ 's', 'u', 'p', 'e', 'r' ];
+
+for (; mauvaisesLettres.length < MAX_TENTATIVES && lettresAttendues.length > 0; ) {
+  console.log('lettres à trouver:', lettresAttendues);
+  alert('Lettres à trouver: ' + lettresAttendues.length + '. '
+    + 'Tentatives restantes: ' + (MAX_TENTATIVES - mauvaisesLettres.length));
   var lettre = prompt('Devinez une lettre');
-  var indice = lettres.indexOf(lettre);
-  if (indice !== -1) {
-    lettres.splice(indice, 1); // retire la 1ère occurrence de cette lettre trouvée dans le tableau
+  var indice = lettresAttendues.indexOf(lettre);
+  if (indice != -1) {
+    lettresAttendues.splice(indice, 1); // retire la 1ère occurrence de cette lettre trouvée dans le tableau
     alert('Bonne pioche! Continuez!');
   } else {
-    --tentatives;
-    alert('Le mot à trouver ne contient pas la lettre ' + lettre+ ', désolé...');
+    mauvaisesLettres.push(lettre);
+    alert('Le mot à trouver ne contient pas la lettre ' + lettre + ', désolé...');
   }
 }
 
-if (lettres.length === 0) {
-  alert('bravo!');
-} else if (tentatives === 0) {
-  alert('perdu!');
+if (lettresAttendues.length == 0) {
+  alert('Bravo, vous avez trouvé le mot !');
+} else if (mauvaisesLettres.length == MAX_TENTATIVES) {
+  alert('Perdu... Faites une autre partie !');
 }
