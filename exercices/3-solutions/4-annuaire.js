2a4296cf2a4a44b464039121128f0c38bc6f780f
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2018-02-06 15:33:07 +0100

    enrichissement de l'exercice annuaire telephonique

--- exercices/3-solutions/4-annuaire.js
+++ exercices/3-solutions/4-annuaire.js
@@ -1,7 +1,13 @@
 var annuaire = {
-  'luke' : '0666666666',
-  'yoda' : '0688888888',
+  patricia: '06 66 66 66 66',
+  david: '07 77 77 77 77',
 };
 
+// 1. Afficher dans la console le numéro de téléphone de `patricia`, en utilisant la notation pointée sur l'objet `annuaire`;
+
+console.log(annuaire.patricia);
+
+// 2. Demander à l'utilisateur de saisir un prénom, puis afficher le numéro de téléphone associé à ce prénom.
+
 var nom = prompt('donner le nom d\'un ami');
-alert('numéro associé: ' + annuaire[nom]);
+alert('numéro de téléphone de ' + nom + ': ' + annuaire[nom]);
